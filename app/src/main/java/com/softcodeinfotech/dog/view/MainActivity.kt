43683ba05d279eab6eaa49ffe9e91ca12689d305
searchmanish <search.manish16@gmail.com>
2019-09-29 14:53:56 +0530

    Initial commit

---
+++ app/src/main/java/com/softcodeinfotech/dog/view/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.softcodeinfotech.dog.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.softcodeinfotech.dog.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
